12325bc0afa926a1a958e5c19780b91b5e1afe0d
Thuan <thuantd2001hb@gmail.com>
2023-06-01 03:12:12 +0700

    menu

---
+++ Assets/Scripts/Gameplay/Menu/OpenMenuBuilderBase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenuBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Canvas canvas;
+    void Start()
+    {
+        
+    }
+    public void LoadShop()
+    {
+        if (!canvas.gameObject.activeSelf)
+        {
+            canvas.gameObject.SetActive(true);
+
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Gameplay/TowerDefend/BuilderBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilderBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Canvas canvas;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadShop()
+    {
+        if (!canvas.gameObject.activeSelf)
+        {
+            canvas.gameObject.SetActive(true);
+
+        }
+
+    }
+    void OnMouseDown()
+    {
+        //Vector3 oldObjectPosition = transform.position;
+        //Quaternion oldObjectRotation = transform.rotation;
+        LoadShop();
+    }
+   
+}
