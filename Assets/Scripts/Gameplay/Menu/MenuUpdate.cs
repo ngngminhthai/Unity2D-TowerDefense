4da863e28024be16a2616d64d5fca67e0060a6cc
Thuan <thuantd2001hb@gmail.com>
2023-06-06 01:05:46 +0700

    done block menu

--- Assets/Scripts/Gameplay/Menu/MenuUpdate.cs
+++ Assets/Scripts/Gameplay/Menu/MenuUpdate.cs
@@ -8,13 +8,15 @@ public class MenuUpdate : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    Canvas canvas;
+    Canvas updateMenuAction;
 
-   
+    [SerializeField]
+    Canvas buyMenu;
+
 
     [SerializeField]
     TextMeshProUGUI LevelText;
-
+    [SerializeField]
     public Button buttonToDisable;
 
    
@@ -25,13 +27,14 @@ public class MenuUpdate : MonoBehaviour
 
 
     GameObject objectClick;
+    [SerializeField]
     int currentLevel;
 
     void Start()
     {
         mainCamera = Camera.main;
       
-        canvas.gameObject.SetActive(false);
+        updateMenuAction.gameObject.SetActive(false);
         //LevelText.text = "Level: " + levelUp;
 
     }
@@ -47,8 +50,8 @@ public class MenuUpdate : MonoBehaviour
                 Vector2 rayOrigin = mainCamera.ScreenToWorldPoint(Input.mousePosition);
                 // Phát ra Ray từ vị trí con trỏ chuột và kiểm tra va chạm với Collider2D
                 hit = Physics2D.RaycastAll(rayOrigin, Vector2.zero);
-
-                if (hit.Length > 0)
+                bool isOverUI = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+                if (hit.Length > 0 && !isOverUI )
                 {
                     for (int i = 0; i < hit.Length; i++)
                     {
@@ -61,6 +64,14 @@ public class MenuUpdate : MonoBehaviour
                             LoadShop();
                             break;
                         }
+                        //Debug.Log("a");
+                        if (hit[i].collider.tag == "BuilderBase")
+                        {
+                            objectClick = hit[i].collider.gameObject;
+                            LoadShop2();
+                            break;
+                        }
+
                     }
                     previousChangeCharacterInput = true;
                 }
@@ -76,11 +87,18 @@ public class MenuUpdate : MonoBehaviour
         }
     }
     public void LoadShop()
-    {
-        if (!canvas.gameObject.activeSelf)
-        {
-            canvas.gameObject.SetActive(true);
 
+    {
+        if (!updateMenuAction.gameObject.activeSelf)
+        {
+            
+
+            updateMenuAction.gameObject.SetActive(true);
+
+        }
+        if (currentLevel == 3)
+        {
+            buttonToDisable.interactable = false;
         }
 
     }
@@ -93,23 +111,45 @@ public class MenuUpdate : MonoBehaviour
     //}
     public void ExitMenu()
     {
-        canvas.gameObject.SetActive(false);
+        updateMenuAction.gameObject.SetActive(false);
 
     }
     public void UpdateMenu()
     {
+        
         Debug.Log("In");
         if (currentLevel == 1)
             Instantiate(tower.towerLevel02, objectClick.transform.position, objectClick.transform.rotation);
-        if(currentLevel==2)
+        ExitMenu();
+        Destroy(objectClick);
+        if (currentLevel==2)
             Instantiate(tower.towerLevel03, objectClick.transform.position, objectClick.transform.rotation);
         ExitMenu();
         Destroy(objectClick);
+        if (currentLevel == 3)
+        {
+            buttonToDisable.interactable = false;
+        }
+      
         
     }
     public void DestroyMenu()
     {
         Instantiate(tower.BuiderBase, objectClick.transform.position, objectClick.transform.rotation);
         Destroy(objectClick);
+    }
+
+
+    public void LoadShop2()
+    {
+        if (!buyMenu.gameObject.activeSelf)
+        {
+           buyMenu.gameObject.SetActive(true);
+
+            // Sử dụng biến objectClick để lấy position của game object được bấm vào
+            BuiderMenuManager.buildPosition = objectClick.transform.position;
+            BuiderMenuManager.destroyBuilderBase = objectClick;
+        }
+
     }
 }
