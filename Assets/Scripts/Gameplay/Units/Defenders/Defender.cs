b56ca95e60ced0ad29d66b9c6d06762231c1e3e0
nguyenminhthaijs <thainguyenminh441@gmail.com>
2023-06-20 21:06:49 +0700

    Make ground unit can't attack flying unit

--- Assets/Scripts/Gameplay/Units/Defenders/Defender.cs
+++ Assets/Scripts/Gameplay/Units/Defenders/Defender.cs
@@ -34,83 +34,90 @@ namespace Assets.Scripts.Gameplay.Units
         //}
         public void OnTriggerStay2D(Collider2D other)
         {
-            if (other.CompareTag("attackers") && currentTarget == null)
+            if (gameObject.GetComponent<Warrion>() is Warrion && other.GetComponent<Harpy>() is Harpy)
             {
 
-                AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
-
-                //currentTarget = other.GetComponent<Attacker>();
-                agent.SetTargetWhenStaying(other.gameObject);
-                currentTarget = other.GetComponent<Attacker>();
-                return;
-            }
-
-            // Check if the collider belongs to a Defender unit
-
-            if (currentTarget != null && currentTarget.tag == "attackers")
-            {
-                AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
-
-                if (GameClickManager.selectedTarget == gameObject)
-                {
-                    if(HitPoints > 0f)
-                    agent.ContinueMoving();
-                }
-                else if (Vector2.Distance(transform.position, currentTarget.transform.position) <= attackRange)
-                {
-                    if (HitPoints > 0f)
-                    {
-                        agent.StopMoving();
-                        stateAttack = 2;
-                        float threshold = 0.1f;
-                        Vector3 FlipDirection = currentTarget.transform.position - transform.position;
-                        float dotProduct = Vector3.Dot(FlipDirection, transform.right);
-                        if (dotProduct < -threshold)
-                        {
-                            transform.localScale = new Vector3(-1, 1, 1);
-                        }
-                        else if (dotProduct > threshold)
-                        {
-                            transform.localScale = new Vector3(1, 1, 1);
-                        }
-                        //gameObject.GetComponent<AgentMoventMent>().isMoving = false;
-                    }
-                }
             }
             else
             {
-                if (GameClickManager.selectedTarget == gameObject)
+                if (other.CompareTag("attackers") && currentTarget == null)
                 {
 
+                    AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
+
+                    //currentTarget = other.GetComponent<Attacker>();
+                    agent.SetTargetWhenStaying(other.gameObject);
+                    currentTarget = other.GetComponent<Attacker>();
+                    return;
                 }
-                else if (currentTarget == null && !other.gameObject.CompareTag("defenders") && !other.gameObject.CompareTag("tower"))
+
+                // Check if the collider belongs to a Defender unit
+
+                if (currentTarget != null && currentTarget.tag == "attackers")
                 {
                     AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
-                    stateAttack = 0;
-                    agent.SetAgentPosition();
+
+                    if (GameClickManager.selectedTarget == gameObject)
+                    {
+                        if (HitPoints > 0f)
+                            agent.ContinueMoving();
+                    }
+                    else if (Vector2.Distance(transform.position, currentTarget.transform.position) <= attackRange)
+                    {
+                        if (HitPoints > 0f)
+                        {
+                            agent.StopMoving();
+                            stateAttack = 2;
+                            float threshold = 0.1f;
+                            Vector3 FlipDirection = currentTarget.transform.position - transform.position;
+                            float dotProduct = Vector3.Dot(FlipDirection, transform.right);
+                            if (dotProduct < -threshold)
+                            {
+                                transform.localScale = new Vector3(-1, 1, 1);
+                            }
+                            else if (dotProduct > threshold)
+                            {
+                                transform.localScale = new Vector3(1, 1, 1);
+                            }
+                            //gameObject.GetComponent<AgentMoventMent>().isMoving = false;
+                        }
+                    }
                 }
-                stateAttack = 0;
-                /*
-                                if (HitPoints > 0f)
-                                    agent.ContinueMoving();*/
-                // AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
-                // stateAttack = 0;
-                /* stateAttack = 0;
-                 if (agent.isMoving == true)
-                 {
-                     agent.SetAgentPosition();
-                 }*/
-
-
-            }
-            Attacker attacker = other.GetComponent<Attacker>();
-            if (attacker != null && (currentTarget == null || attacker == currentTarget))
-            {
-                //currentTarget = attacker;
-                // đến tầm mới bắn
-                if (Vector2.Distance(transform.position, attacker.transform.position) <= attackRange)
+                else
                 {
-                    Attack(attacker);
+                    if (GameClickManager.selectedTarget == gameObject)
+                    {
+
+                    }
+                    else if (currentTarget == null && !other.gameObject.CompareTag("defenders") && !other.gameObject.CompareTag("tower"))
+                    {
+                        AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
+                        stateAttack = 0;
+                        agent.SetAgentPosition();
+                    }
+                    stateAttack = 0;
+                    /*
+                                    if (HitPoints > 0f)
+                                        agent.ContinueMoving();*/
+                    // AgentMoventMent agent = gameObject.GetComponent<AgentMoventMent>();
+                    // stateAttack = 0;
+                    /* stateAttack = 0;
+                     if (agent.isMoving == true)
+                     {
+                         agent.SetAgentPosition();
+                     }*/
+
+
+                }
+                Attacker attacker = other.GetComponent<Attacker>();
+                if (attacker != null && (currentTarget == null || attacker == currentTarget))
+                {
+                    //currentTarget = attacker;
+                    // đến tầm mới bắn
+                    if (Vector2.Distance(transform.position, attacker.transform.position) <= attackRange)
+                    {
+                        Attack(attacker);
+                    }
                 }
             }
         }
