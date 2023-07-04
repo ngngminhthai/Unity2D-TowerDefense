81f87e8339b53220af4a7c823b206e959445fcd9
Thuan <thuantd2001hb@gmail.com>
2023-07-05 00:28:29 +0700

    skill

---
+++ Assets/Resources/Scripts/Gameplay/Map/Frame.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxHealth = 10;
+    public float healingRate = 2;
+    public float radius = 2f;
+    Timer timer;
+
+    private float currentHealth;
+
+    void Start()
+    {
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = 200;
+        timer.Run();
+        currentHealth = maxHealth;
+        InvokeRepeating("Effect", 0.0f, 2f);
+    }
+
+    public virtual void Effect()
+    {
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+     
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject.CompareTag("defenders")|| collider.gameObject.CompareTag("attackers"))
+            {
+                Unit unit = collider.GetComponent<Unit>();
+                if (unit != null)
+                {
+                    unit.TakeDamage(healingRate);
+                    //var animation = GameObject.Instantiate(rangeAnimation, gameObject.transform.position, Quaternion.identity);
+                    //Tower.colliders.Remove(collider.gameObject);
+                    //OnDrawGizmosSelected();
+                }
+
+            }
+        }
+    }
+    void Update()
+    {
+        if (timer.Finished)
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Resources/Scripts/Gameplay/Skill/Freeze.cs
+++ Assets/Resources/Scripts/Gameplay/Skill/Freeze.cs
@@ -1,4 +1,4 @@
-using Assets.Scripts.Gameplay.Units;
+﻿using Assets.Scripts.Gameplay.Units;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,46 +8,140 @@ public class Freeze : MonoBehaviour
     // Start is called before the first frame update
     public float radius = 3f;
     Timer timer;
+    private List<GameObject> objectsInTrigger = new List<GameObject>();
     void Start()
     {
         timer = gameObject.AddComponent<Timer>();
-        timer.Duration = 4;
+        timer.Duration = 20f;
         timer.Run();
-        Invoke("FreezeObjects", 0.0f);
+        //Invoke("FreezeObjects", 0.0f);
 
     }
 
-    void FreezeObjects()
+    //void FreezeObjects()
+    //{
+
+    //    Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+    //    if (colliders.Length > 0) Debug.Log("x");
+    //    foreach (Collider2D collider in colliders)
+    //    {
+    //        if (collider.gameObject.CompareTag("attackers"))
+    //        {
+    //            AgentMoventMentMonster agent = collider.GetComponent<AgentMoventMentMonster>();
+
+    //            if (agent != null && !agent.isAccessMovingTower)
+    //            {
+    //                float speed = agent.GetSpeed();
+    //                //agent.AdjustSpeed((float)0.5);
+    //                agent.AdjustSpeed((float)0.0, 3f);
+    //                float speedAfter = agent.GetSpeed();
+
+    //            }
+    //        }
+    //    }
+    //}
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
-        if (colliders.Length > 0) Debug.Log("x");
-        foreach (Collider2D collider in colliders)
+        // Kiểm tra nếu một đối tượng khác đi qua trigger
+        if (other.CompareTag("attackers"))
         {
-            if (collider.gameObject.CompareTag("attackers"))
-            {
-                AgentMoventMentMonster agent = collider.GetComponent<AgentMoventMentMonster>();
+           
+            AgentMoventMentMonster agent = other.GetComponent<AgentMoventMentMonster>();
+            //AgentMoventMent agent = other.GetComponent<AgentMoventMentMonster>();
 
-                if (agent != null && !agent.isAccessMovingTower)
-                {
-                    float speed = agent.GetSpeed();
-                    //agent.AdjustSpeed((float)0.5);
-                    agent.AdjustSpeed((float)0.0, 3f);
-                    float speedAfter = agent.GetSpeed();
-                    
-                }
+            if (agent != null && !agent.isAccessMovingTower)
+            {
+               // Debug.Log("aggent");
+                float speed = agent.GetSpeed();
+                //agent.AdjustSpeed((float)0.5);
+                agent.AdjustSpeed((float)8.0f);
+
+                float speedAfter = agent.GetSpeed();
+
+                // Xử lý tương tác khi đối tượng đi qua trigger
+                //Debug.Log("Object entered the trigger:");
             }
+            // Gọi hàm xử lý khác tại đây
+        }
+        else if (other.CompareTag("defenders"))
+        {
+            Debug.Log("def");
+            AgentMoventMent agent = other.GetComponent<AgentMoventMent>();
+
+            if (agent != null)
+            {
+                Debug.Log("aggent");
+                float speed = agent.GetSpeed();
+                //agent.AdjustSpeed((float)0.5);
+                agent.AdjustSpeed((float)10.0f);
+
+                float speedAfter = agent.GetSpeed();
+
+            }
+
+
+            // Xử lý tương tác khi đối tượng đi qua trigger
+            Debug.Log("Object entered the trigger:");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Kiểm tra nếu một đối tượng khác đi qua trigger
+        if (other.CompareTag("attackers"))
+        {
+
+            AgentMoventMentMonster agent = other.GetComponent<AgentMoventMentMonster>();
+            //AgentMoventMent agent = other.GetComponent<AgentMoventMentMonster>();
+
+            if (agent != null && !agent.isAccessMovingTower)
+            {
+                // Debug.Log("aggent");
+                float speed = agent.GetSpeed();
+                //agent.AdjustSpeed((float)0.5);
+                agent.AdjustSpeed((float)2f);
+
+                float speedAfter = agent.GetSpeed();
+
+                // Xử lý tương tác khi đối tượng đi qua trigger
+                //Debug.Log("Object entered the trigger:");
+            }
+            // Gọi hàm xử lý khác tại đây
+        }
+        else if (other.CompareTag("defenders"))
+        {
+            Debug.Log("def");
+            AgentMoventMent agent = other.GetComponent<AgentMoventMent>();
+
+            if (agent != null)
+            {
+                Debug.Log("aggent");
+                float speed = agent.GetSpeed();
+                //agent.AdjustSpeed((float)0.5);
+                agent.AdjustSpeed((float)2f);
+
+                float speedAfter = agent.GetSpeed();
+
+            }
+
+
+            // Xử lý tương tác khi đối tượng đi qua trigger
+            Debug.Log("Object entered the trigger:");
+        }
+    }
+    void Update()
+    {
+        if (timer.Finished)
+        {
+
+            Destroy(gameObject);
+
         }
     }
 
-    void Update()
-    {
-        //if (timer.Finished)
-        //{
-
-        //    Destroy(gameObject);
-
-        //}
-    }
-
 }
+
+
+
+
+
+
